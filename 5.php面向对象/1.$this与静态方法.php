3b297b25e6e5d2fd8fa5a1c91157a20d496023e0
liukun <13655914562@163.com>
2023-09-26 16:53:34 +0800

    oop-1

---
+++ 5.php面向对象/1.$this与静态方法.php
@@ -0,0 +1,23 @@
+<?php
+class User
+{
+    protected $name = 'liukun';
+    protected static $class = '209';
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function say()
+    {
+        // return User::$class . $this->getName() . '說:hello world'; 
+        //静态变量方法等属于类本身，不能使用$this，只能使用User::$class
+        // return self::$class . $this->getName() . '說:hello world'; //self表示当前类
+        return self::getClass();
+    }
+    public static function getClass(){
+        return self::$class;
+    }
+}
+
+$obj = new User();
+echo $obj->say();
